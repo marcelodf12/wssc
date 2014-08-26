14bc06258503d10aaf900b2139550f5a6f035d8f
marcelo <marcelo@marcelo-franco>
2014-08-26 16:42:10 -0400

    apps correccion

--- src/main/webapp/js/apps.js
+++ src/main/webapp/js/apps.js
@@ -67,10 +67,16 @@ Wssc.config(['$routeProvider', function($routeProvider) {
                     templateUrl: 'movimientos/registrar_compra.html',
                     controller: 'compraAltaCtrl'
                 })
+                .when('/movimientos/compras/listar', {
+                    templateUrl: 'movimientos/listar_compras.html',
+                    controller: 'comprasListarCtrl'
+                })
                 .when('/movimientos/ventas/registrar', {
                     templateUrl: 'movimientos/registrar_venta.html',
                     controller: 'MainController'
                 })
+                
+                
 
 
                 .otherwise({RedirectTo: '/'});
@@ -192,26 +198,71 @@ Wssc.controller('proveedorEliminarCtrl', function($scope, $http, $routeParams, $
     };
 });
 
+
+
+
 // Controladores para compra
 Wssc.controller('compraAltaCtrl', function($scope, $http, $location) {
     var url = "webresources/pol.una.py.wssc.proveedores/";
+    var productos = [];
     $scope.cantidad = [];
+    $scope.precio = [];
+    $scope.stock = [];
+    var cant_p = 0;
+    var proveedor;
     $scope.consultar = function() {
         var consulta = url + "productos/" + $scope.proveedor;
         $scope.productos = {};
         $scope.cantidad = [];
+        $scope.precio = [];
+        $scope.stock = [];
         $http.get(consulta)
                 .success(function(response) {
                     $scope.productos = response;
+                    productos = $scope.productos;
+                    cant_p = productos.length;
+                    $http.get(url + $scope.proveedor).success(function(response) {
+                        proveedor = response;
+                        console.log(proveedor);
+                    });
                 });
     };
 
     $scope.alta = function() {
-        var url = "webresources/pol.una.py.wssc.clientes/";
-        console.log("inicio del bucle");
-        for(var i=1; i<$scope.cantidad.length;i++){
-            console.log($scope.cantidad[i]);
+        var url = "webresources/pol.una.py.wssc.compras/";
+        var f = new Date();
+        var ano = f.getFullYear();
+        var mes = f.getMonth();
+        var dia = f.getDate();
+        var compra = {
+            "fecha": ano + "-" + mes + "-" + dia,
+            "fkProveedor": proveedor
+        };
+        for (var i = 0; i < cant_p; i++) {
+            id = productos[i].id;
+            if (typeof ($scope.cantidad[id]) !== 'undefined') {
+                if ($scope.cantidad[id].valueOf() !== 0) {
+                    if (productos[i].stock >= $scope.cantidad[id]) {
+                        nuevoDetalleCompra = {
+                            "cantidad": parseInt($scope.cantidad[id]),
+                            "fkProducto": productos[i],
+                            "precio": productos[i].precio,
+                            "fkCompra":compra
+                        };
+                    console.log(nuevoDetalleCompra);
+                    dir = "webresources/pol.una.py.wssc.detallecompra/";
+                    $http.post(dir, nuevoDetalleCompra);
+                    }
+                }
+            }
         }
-        console.log("fin del bucle");
+        console.log(compra);
+        $http.post(url, compra);
     };
+});
+Wssc.controller('comprasListarCtrl', function($scope, $http) {
+    $http.get("webresources/pol.una.py.wssc.compras/")
+            .success(function(response) {
+                $scope.compras = response;
+            });
 });
